46d4b061cfac5145b76979be3919a45ac059cfb4
Jullyene Patta Ramos <jilesss9023Caju@outlook.com>
2025-02-28 23:24:03 -0300

    Exercício 2 da Aula 3

--- Aula3/exercicio2.py
+++ Aula3/exercicio2.py
@@ -1,3 +1,5 @@
+
+
 """
     Analisador de Texto 
 
@@ -15,3 +17,21 @@
 
 
 """
+import re
+from collections import Counter
+
+def analisar_texto(texto):
+    stopwords = {"de", "o", "a", "e", "que", "com", "do", "da", "em", "um", "para", "se", "na", "no"}
+    
+    texto_limpo = re.sub(r'[^\w\s]', '', texto.lower()) 
+    palavras = texto_limpo.split() 
+    palavras_filtradas = [palavra for palavra in palavras if palavra not in stopwords] 
+    
+    contagem = Counter(palavras_filtradas)  
+    mais_comuns = contagem.most_common(5) 
+    
+    return mais_comuns
+
+# Exemplo de uso:
+texto = "Só sei que nada sei e o que não sei aprenderei! E dito o que aprenderei aperfeiçoarei!"
+print(analisar_texto(texto))
